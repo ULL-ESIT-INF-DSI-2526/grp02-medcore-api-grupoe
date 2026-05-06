79919738ffed11183ee5741e1dbf96e7b6bf23e4
Mario Martín Ramón <144797314+MarioMartinRamon@users.noreply.github.com>
2026-05-06 18:54:05 +0000

    Creación de modelo MEDICATIONS

---
+++ src/models/medications.ts
@@ -0,0 +1,84 @@
+import { Document, Schema, model } from 'mongoose';
+import validator from 'validator';
+
+export type PharmaceuticalForm = 'comprimido' | 'cápsula' | 'solución oral' | 'solución inyectable' | 'pomada' | 'parche transdérmico' | 'inhalador' | 'otras';
+export type AdministrationRoute = 'oral' | 'intravenosa' | 'intramuscular' | 'subcutánea' | 'tópica' | 'inhalatoria';
+
+export interface MedicationsDocumentInterface extends Document {
+  commercialName: string;
+  activeIngredient: string;
+  nationalCode: string;
+  pharmaceuticalForm: PharmaceuticalForm;
+  standardDose: number;
+  doseUnit: string;
+  administrationRoute: AdministrationRoute;
+  stock: number;
+  unitPrice: number;
+  requiredPrescription: boolean;
+  expirationDate: Date;
+  contraindications: string[];
+}
+
+const MedicationsSchema = new Schema<MedicationsDocumentInterface>({
+  commercialName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+  },
+  activeIngredient: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+  },
+  nationalCode: {
+    type: String,
+    unique: true, // Debe ser único
+    required: true,
+    trim: true,
+  },
+  pharmaceuticalForm: {
+    type: String,
+    required: true,
+    enum: ['comprimido', 'cápsula', 'solución oral', 'solución inyectable', 'pomada', 'parche transdérmico', 'inhalador', 'otras'],
+  },
+  standardDose: {
+    type: Number,
+    required: true,
+    min: 0,  // Evito negativos
+  },
+  doseUnit: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  administrationRoute: {
+    type: String,
+    required: true,
+    enum: ['oral', 'intravenosa', 'intramuscular', 'subcutánea', 'tópica', 'inhalatoria']
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0, // Evito negativos
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+    min: 0, // Evito negativos
+  },
+  requiredPrescription: {
+    type: Boolean,
+    required: true,
+  },
+  expirationDate: {
+    type: Date,
+    required: true,
+  },
+  contraindications: {
+    type: [String],
+  }
+});
+
+export const MedicationsModel = model<MedicationsDocumentInterface>('Medications', MedicationsSchema);
