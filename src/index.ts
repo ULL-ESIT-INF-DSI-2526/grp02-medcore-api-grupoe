2f4e5aec9e69a28c4bb6af89a799c4e32a2aa457
Abel Martín Meneses <alu0101660011@ull.edu.es>
2026-05-10 12:43:13 +0100

    Arreglo index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-import express from 'express';
-import './db/mongoose.js';
-import { patientRouter } from './routers/patient.js';
-import { staffRouter } from './routers/staff.js';
-import { medicationsRouter } from './routers/medications.js';
-import { defaultRouter } from './routers/default.js';
-import { recordsRouter } from './routers/records.js';
-
-const app = express();
-app.use(express.json());
-app.use(patientRouter);
-app.use(staffRouter);
-app.use(medicationsRouter);
-app.use(recordsRouter);
-app.use(defaultRouter);
+import { app } from './app.js';
 
 const port = process.env.PORT || 3000;
 
